4e43dd1dc45be8c74168530c3f8af092b1a55615
allenvarughese22 <allenvrghs2007@gmail.com>
2023-12-14 14:11:06 -0500

    changed the whole code

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 id="heading">Allen Varughese</h1>
-      <h2>Sematics project</h2>
-      <br />
-      <p class="para">Project to change and make minor changes and submit</p>
-      <h3> Things we are learning in this session:</h3>
-      <ul>
-        <li>
-          <i>javascript</i>
-        </li>
-        <li> react</li>
-      </ul>
-      <div>
-        <h4> for reference: pls use the below lings</h4>
-        Go to Google
-      </div>
-      <br />
-      <div>
-        <b> keywords used in javascript</b>
-      </div>
+      <h1> hello today is not tuesday</h1>
     </>
   );
 }
