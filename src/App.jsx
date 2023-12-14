bdc1dfd7e804459ef38c5e1aa104a364dcade266
allenvarughese22 <allenvrghs2007@gmail.com>
2023-12-14 14:03:30 -0500

    double <>

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from "react";
-
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <>
-        <h1 id="heading">Allen Varughese</h1>
-        <h2>Sematics project</h2>
-        <br />
-        <p class="para">Project to change and make minor changes and submit</p>
-        <h3> Things we are learning in this session:</h3>
-        <ul>
-          <li>
-            <i>javascript</i>
-          </li>
-          <li> react</li>
-        </ul>
-        <div>
-          <h4> for reference: pls use the below lings</h4>
-          Go to Google
-        </div>
-        <br />
-        <div>
-          <b> keywords used in javascript</b>
-        </div>
-      </>
+      <h1 id="heading">Allen Varughese</h1>
+      <h2>Sematics project</h2>
+      <br />
+      <p class="para">Project to change and make minor changes and submit</p>
+      <h3> Things we are learning in this session:</h3>
+      <ul>
+        <li>
+          <i>javascript</i>
+        </li>
+        <li> react</li>
+      </ul>
+      <div>
+        <h4> for reference: pls use the below lings</h4>
+        Go to Google
+      </div>
+      <br />
+      <div>
+        <b> keywords used in javascript</b>
+      </div>
     </>
   );
 }
