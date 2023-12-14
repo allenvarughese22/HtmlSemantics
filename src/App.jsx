a0e23f620e726f3de8eb6f2481e40e70b04a2a58
allenvarughese22 <allenvrghs2007@gmail.com>
2023-12-14 12:03:27 -0500

    added the code and deleted the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <>
+        <h1 id="heading">Allen Varughese</h1>
+        <h2>Sematics project</h2>
+        <br />
+        <p class="para">Project to change and make minor changes and submit</p>
+        <h3> Things we are learning in this session:</h3>
+        <ul>
+          <li>
+            <i>javascript</i>
+          </li>
+          <li> react</li>
+        </ul>
+        <div>
+          <h4> for reference: pls use the below lings</h4>
+          <a href="https://www.google.com"> google Link </a>
+          Go to Google
+        </div>
+        <br />
+        <div>
+          <b> keywords used in javascript</b>
+          <img src="https://miro.medium.com/v2/resize:fit:1400/1*4UNw4zrgC2cdh9Voon6K0A.png"></img>
+        </div>
+      </>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
